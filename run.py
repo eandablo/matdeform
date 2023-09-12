d18144e56b1b547261a0ad76e3464ad32e63a9b6
eandablo <>
2023-09-12 13:46:11 +0000

    initial commit

--- run.py
+++ run.py
@@ -1,3 +1,15 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+def welcome_message():
+    """
+    Writes the initial message of the site
+    """
+    print('Welcome to MATDEFORM')
+    print('You will race a car vs the computer')
+
+
+def main():
+    welcome_message()
+
+main()
