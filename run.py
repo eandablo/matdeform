81212492a3a0454a53b10ca7dc25580cc7cf29cf
eandablo <>
2023-09-13 16:39:56 +0000

    accept entries from user and validate them to be numbers from 1 to 3

--- run.py
+++ run.py
@@ -3,20 +3,22 @@
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 from numpy import random
 
-def run_game():
+def start_game():
     """
     Writes the initial message of the site
+    calculates the first move given by the computer using a random generator
     """
-    print('Welcome to 3Dtic')
     print('This is a 3D tic tac toe game, it consist of three floors of the classic game')
-    print('In order to win you need to form a straight line in any direction')
+    print('You need to win conventianally 2 of the 3 floors or connecting the 3 floors with a vertival line, by choosing a move 1 floor at the time')
     print('You will play against the machine, empty spots are marked as O')
-    print('The machine moves are marked as M, Your moves are marked as Y, Machine moves first')
+    print('The machine moves are marked as M, your moves are marked as Y. Machine moves first')
     first_machine_move()
 
 def first_machine_move():
     """
     creates a random move from the machine
+    assigns the 
+    prints the first outline with the first move from the computer
     """
     floor_random=random.randint(3,size=3)
     floors[floor_random[0]].assign_value(floor_random[1],floor_random[2],'M')
@@ -26,13 +28,36 @@ def first_machine_move():
 def get_user_move():
     """
     gets the user entries for positions
-    asks for floor, x and y
-    later validates
+    asks for floor number, x position and y position
+    later validates the entries
     """
-    print('Please chose your move')
-    print('Start with the floor number')
-    print('0 for Bottom, 1 for mid or 2 for top')
-    floor = input('Enter floor:/n')
+    print('Please chose your move, providing 3 numbers from 1 to 3')
+    while True:
+        floor = input('Enter floor number, 1 for Bottom, 2 for mid or 3 for top:\n')
+        if validate_number(floor):
+            break
+    while True:
+        horizontal = input('Enter the horizontal position:\n')
+        if validate_number(horizontal):
+            break
+    while True:
+        vertical = input('Enter the vertical position:\n')
+        if validate_number(vertical):
+            break
+
+def validate_number(num):
+    """
+    Checks that entry is a number between 1 and 3
+    """
+    try:
+        int(num)
+        if int(num)<1 or int(num)>3:
+            raise ValueError('Value must be a number between 1 and 3')
+    except ValueError as e:
+        print(f'Invalid position:{e}')
+        return False
+    return True
+
 
 class GameFloor:
     """
@@ -63,7 +88,7 @@ floors=[GameFloor('Top'),GameFloor('Mid'),GameFloor('Bottom')]
 
 
 def main():
-    run_game()
+    start_game()
+    get_user_move()
 
-    
 main()
