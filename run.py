fc1a78fe06fe552dfeb1b8e2c65e16f04fd60294
eandablo <>
2023-09-20 13:59:20 +0000

    divided find critical functions

--- run.py
+++ run.py
@@ -207,17 +207,21 @@ def empty_point(data):
 def machine_intel_move():
     """
     Decides the next move from the machine by analising the floors
-    Movement priority is as follows
-      1. Looks for potential winning lines, if none
-      2. Looks for potential losing lines, if none
-      3. Makes a move based on a Bayes like algorithm
     Calls the update function with the selected move
     """
-    win_move = find_critical_by_floor('M')  # winning move by floor
+    win_move = find_critical_row_floor('M', 'Y')  # winning move by floor
     if win_move:
         update_floor(win_move, 'M')
         return
-    block_move = find_critical_by_floor('Y')  # blocking move by floor
+    win_move = find_critical_diagonal_floor('M', 'Y')
+    if win_move:
+        update_floor(win_move, 'M')
+        return
+    block_move = find_critical_row_floor('Y', 'M')  # blocking move by floor
+    if block_move:
+        update_floor(block_move, 'M')
+        return
+    block_move = find_critical_diagonal_floor('Y', 'M')
     if block_move:
         update_floor(block_move, 'M')
         return
@@ -233,42 +237,52 @@ def machine_intel_move():
     update_floor(move, 'M')
 
 
-def find_critical_by_floor(mover):
+def find_critical_row_floor(mover, contender):
     """
     Find rows and lines with two spaces marked by contender
     finds if this row already contains a space filled by the mover
     if not, suggests a move to this empty space.
     """
-    if mover == 'M':
-        contender = 'Y'
-    else:
-        contender = 'M'
     floor_num = 0
     for floor in floors:
         lines_sum = summarize_floor(floor.floor_squares, mover)
         ver_sum = lines_sum[:3]
         hor_sum = lines_sum[3:6]
-        trace = lines_sum[6]
-        antitrace = lines_sum[7]
         lines_sum_contender = summarize_floor(floor.floor_squares, contender)
         ver_sum_contender = lines_sum_contender[:3]   # sum of columns
         hor_sum_contender = lines_sum_contender[3:6]  # sum of rows
-        trace_contender = lines_sum_contender[6]
-        antitrace_contender = lines_sum_contender[7]
         if 2 in ver_sum:
             index_mover = np.where(ver_sum == 2)[0]
-            for index_2 in index_mover:
-                m_move = find_empty_space_vertical(index_2,
-                                                   ver_sum_contender, floor, floor_num)
+            for index in index_mover:
+                m_move = find_empty_space_vertical(index, ver_sum_contender,
+                                                   floor, floor_num)
             if m_move:
                 return m_move
         if 2 in hor_sum:
             index_mover = np.where(hor_sum == 2)[0]
-            for index_1 in index_mover:
-                m_move = find_empty_space_row(index_1, hor_sum_contender,
+            for index in index_mover:
+                m_move = find_empty_space_row(index, hor_sum_contender,
                                               floor, floor_num)
             if m_move:
                 return m_move
+        floor_num += 1
+    return False
+
+
+def find_critical_diagonal_floor(mover, contender):
+    """
+    Finds diagonals with two spaces marked by contender
+    finds if this row already contains a space filled by the mover
+    if not, suggests a move to this empty space.
+    """
+    floor_num = 0
+    for floor in floors:
+        lines_sum = summarize_floor(floor.floor_squares, mover)
+        trace = lines_sum[6]
+        antitrace = lines_sum[7]
+        lines_sum_contender = summarize_floor(floor.floor_squares, contender)
+        trace_contender = lines_sum_contender[6]
+        antitrace_contender = lines_sum_contender[7]
         if trace == 2:
             m_move = find_empty_space_trace(trace_contender, floor,
                                             floor_num, 't')
@@ -286,6 +300,7 @@ def find_critical_by_floor(mover):
 def find_empty_space_vertical(index, data, floor, num):
     """
     explore the vertical lines in floor to find the empty space
+    returns the empty space 3-d position
     """
     if data[index] == 0:
         for i in range(3):
@@ -299,6 +314,7 @@ def find_empty_space_vertical(index, data, floor, num):
 def find_empty_space_row(index, data, floor, num):
     """
     explore the rows in floor to find the empty space
+    returns the empty space 3-d position
     """
     if data[index] == 0:
         for i in range(3):
@@ -312,6 +328,7 @@ def find_empty_space_row(index, data, floor, num):
 def find_empty_space_trace(data, floor, num, flag):
     """
     explore the trace in floor to find the empty space
+    returns the empty space 3-d position
     """
     if flag == 't':
         sign = 1
@@ -331,6 +348,7 @@ def find_empty_space_trace(data, floor, num, flag):
 def find_critical_column(mover):
     """
     Find columns with potential win for mover
+    returns a 3D position if a empty space is found
     """
     if mover == 'M':
         contender = 'Y'
@@ -342,20 +360,17 @@ def find_critical_column(mover):
         for j in range(3):
             m_move = find_empty_space_column(column_sum, column_sum_contender,
                                              i, j)
-#            if column_sum[i, j] == 2 and column_sum_contender[i, j] == 0:
-#                floor_num = 0
-#                for floor in floors:
-#                    if floor.floor_squares[i][j] == " ":
-#                        next_move = [floor_num, i, j]
-#                        break
-#                    floor_num += 1
-#                return next_move
             if m_move:
                 return m_move
     return False
 
 
 def find_empty_space_column(data, data_contender, i, j):
+    """
+    explore to find the empty space acrooss all floors in
+    a straight line
+    returns the empty space 3-d position
+    """
     if data[i, j] == 2 and data_contender[i, j] == 0:
         floor_num = 0
         for floor in floors:
